7cb36a1fb1e455e78c12221759bfd36ed9682d9b
sidwah <sidwabarrock@gmail.com>
2025-05-01 10:03:23 +0000

    Deploy latest build

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/crispie/', // Add this line to set the base URL for GitHub Pages
 })
-
