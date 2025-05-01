df97c0a48ea6b828247a3ddd23b0d59084678248
sidwah <sidwabarrock@gmail.com>
2025-05-01 10:34:06 +0000

    Fix routing for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
-// src/main.jsx
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';  // Change here
 import './index.css';
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>  {/* Change here */}
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 );
